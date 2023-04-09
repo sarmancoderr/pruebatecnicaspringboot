d4625e0cd4afd52cb0cfc96f0e34ea9526a08514
Raul <raul@liftel.es>
2023-04-09 21:17:00 +0200

    Varios profiles

--- src/test/java/es/sarman/pruebatecnica/Heroes/IHeroServiceTest.java
+++ src/test/java/es/sarman/pruebatecnica/Heroes/IHeroServiceTest.java
@@ -6,14 +6,19 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@RunWith(SpringRunner.class)
+@Profile("test")
 @SpringBootTest
+@ActiveProfiles("test")
+@RunWith(SpringRunner.class)
 class IHeroServiceTest {
 
     @Autowired IHeroService heroService;
