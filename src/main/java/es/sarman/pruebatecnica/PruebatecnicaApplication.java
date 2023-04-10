1b365582d516c654b2335c7bf700cfa510591d0c
Raul <raul@liftel.es>
2023-04-10 12:18:15 +0200

    Mover clase a su respectivo paquete

--- src/main/java/es/sarman/pruebatecnica/PruebatecnicaApplication.java
+++ src/main/java/es/sarman/pruebatecnica/PruebatecnicaApplication.java
@@ -3,15 +3,12 @@ package es.sarman.pruebatecnica;
 import es.sarman.pruebatecnica.Heroes.Hero;
 import es.sarman.pruebatecnica.Heroes.HeroRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +16,6 @@ import java.util.List;
 
 @SpringBootApplication
 @RestController
-@EnableWebSecurity
 @Slf4j
 public class PruebatecnicaApplication {
 
--- src/main/java/es/sarman/pruebatecnica/security/SecurityConfig.java
+++ src/main/java/es/sarman/pruebatecnica/security/SecurityConfig.java
@@ -1,18 +1,18 @@
-package es.sarman.pruebatecnica;
+package es.sarman.pruebatecnica.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
     @Value("${spring.security.user.name}")
     private String user;
